2c0884f0ac36af4b48e7278fa31a7a9eb1a56e0b
Mariana Santos <maririb51@gmail.com>
2024-02-05 12:02:36 +0000

    Adding HATEOAS to the FindById, Insert, and Update methods

--- src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -43,7 +43,11 @@ public class MovieService {
 	public MovieDTO findById(Long id) {
 		MovieEntity result = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new MovieDTO(result);
+		MovieDTO dto = new MovieDTO(result).add(linkTo(methodOn(MovieController.class).findById(id)).withSelfRel())
+				.add(linkTo(methodOn(MovieController.class).findAll(null, null)).withRel("All movies"))
+				.add(linkTo(methodOn(MovieController.class).update(id,null)).withRel("Update movie"))
+				.add(linkTo(methodOn(MovieController.class).delete(id)).withRel("Delete movie"));
+           	return dto;
 	}
 
 	@Transactional(readOnly = true)
@@ -59,7 +63,7 @@ public class MovieService {
 		MovieEntity entity = new MovieEntity();
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
-		return new MovieDTO(entity);
+		return new MovieDTO(entity).add(linkTo(methodOn(MovieController.class).findById(entity.getId())).withRel("Get movie by id"));
 	}
 
 	@Transactional
@@ -68,7 +72,7 @@ public class MovieService {
 			MovieEntity entity = repository.getReferenceById(id);
 			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
-			return new MovieDTO(entity);
+			return new MovieDTO(entity).add(linkTo(methodOn(MovieController.class).findById(entity.getId())).withRel("Get movie by id"));
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Recurso não encontrado");
 		}
